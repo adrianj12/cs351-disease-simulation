567049c5543933765fbd087670f5de3f93b1384d
Adrian Abeyta <ajabeyta@unm.edu>
2022-11-12 22:11:58 -0700

    added GUI class and all IML files in gitignore

---
+++ src/DiseaseSimulation/GUI.java
@@ -0,0 +1,17 @@
+package DiseaseSimulation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage stage) {
+
+        
+
+    }
+
+}
